29207af6626d66252909bea9570f66dea5f08a6a
ASWANI K A <aswanika05@gmail.com>
2025-11-11 16:26:56 +0530

    style: add Tailwind styling for card layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='grid gap-1'>
+     <h1 className='font-semibold text-gray-700'>Profile Cards</h1>
+     <Card img="https://images.pexels.com/photos/3756679/pexels-photo-3756679.jpeg" name="Steffy Johnson" dest="Software Developer" city="Bangalore"
+     />
+     <Card img="https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg" name="Mathew Joy" dest="Project Manager" city="Hyderabad"
+     />
+     <Card img="https://images.pexels.com/photos/3861962/pexels-photo-3861962.jpeg" name="Steffy Johnson" dest="Executive Head" city="Pune"
+     />
+    </div>
   )
 }
 
